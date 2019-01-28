d29bedb0ff7f6d05ca2f2cfa40d24d302fc50f6a
Alicia Wu <ali.ciawu890@gmail.com>
2019-01-28 16:42:59 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,8 +2,6 @@
 def valid_move?(array, input)
   if input.between?(0, 8) == true
     true
-  else
-    false
   end
 end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
